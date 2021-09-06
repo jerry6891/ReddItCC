bf2fe84dc874a9690c15e7ab5fe661b95a961551
Jerry <gweb1986@gmail.com>
2021-09-05 20:04:00 -0500

    Full ReddItCC iOS Project

---
+++ AppDelegate/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  ReddItCC
+//
+//  Created by Rex Karnufex on 9/3/21.
+//
+
+import Foundation
+
+struct Constants {
+    static let baseUrl = "https://www.reddit.com/"
+    static let placeHolderimageName = "ImagePlaceHolder"
+}
